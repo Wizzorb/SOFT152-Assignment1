121a94914a124df15abe51ec75f83f175ceedcc6
Wizzorb <57352516+Wizzorb@users.noreply.github.com>
2019-12-10 00:56:52 +0000

    Working on sorting algorithm

---
+++ SOFTAssignment - 10-12/SOFTAssignment/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SOFTAssignment
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoadData("miniAirBnB.txt");
+        }
+
+        //private WriteFile[] StArray;
+
+        public void LoadData(string readerFile)
+        {
+            StreamReader SReader = new StreamReader(readerFile);
+
+            int inputNum;
+            int nextCharNum;
+            char inputAsChar;
+            string inputString;
+            string dName;
+            int dNeighNum;
+            string nName;
+            int nPropertyNum;
+
+            Districts[] tempDistricts;
+            Neighbourhood[] tempNeigh;
+            Property[] tempProp;
+
+            //Districts d = new Districts(gsdg, sdgds, sdgds);
+
+            try
+            {
+                while (!SReader.EndOfStream)
+                {
+                    dName = SReader.ReadLine();
+                    dNeighNum = Convert.ToInt32(SReader.ReadLine());
+                    for (int i = 0; dNeighNum < i; i++)
+                    {
+                         
+                    }
+                }
+
+            }
+            catch(FileNotFoundException notFound)
+            {
+                MessageBox.Show("ERROR Opening file (when writing data out)" + "- File could not be found.\n" + notFound.Message);
+                //return false;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("ERROR Opening File (when writing data out)" + "- Operation failed.\n" + e.Message);
+                //return false;
+            }
+
+           /*while (!SReader.EndOfStream)
+            {
+                try
+                {
+                    // Data needs to be formatted and assigned to the correct array of classes (which contains the classes of District (2 lines), Neighbourhood (2 lines) and Property (11 lines))
+                    // 
+                    
+                    
+                    //inputString dataOut = new StreamWriter(StArray);
+                    return true;
+                }
+            }*/
+        }
+    }
+}
---
+++ SOFTAssignment - 10-12/SOFTAssignment/Property.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOFTAssignment
+{
+    class Property
+    {
+        private string propID;
+        private string propName;
+        private string hostID;
+        private string hostName;
+        private int hostPropNum;
+        private double Lat;
+        private double Long;
+        private string roomType;
+        private double Price;
+        private int minNights;
+        private int availableDays;
+        public Property(string thePID, string thePName, string theHID, string theHName, string theHPNum, double theLat, double theLong, string theRoomType, double thePrice, string theMinNights, string theAvailDays)
+        {
+            propID = thePID;
+            propName = thePName;
+            hostID = theHID;
+            hostName = theHName;
+            setHPN(theHPNum);
+            setLat(theLat);
+            setLongi(theLong);
+            roomType = theRoomType;
+            Price = thePrice;
+            setminN(theMinNights);
+            setavailableD(theAvailDays);
+        }
+
+        //Get Methods
+        public string getPID()
+        {
+            return propID;
+        }
+        public string getPName()
+        {
+            return propName;
+        }
+        public string getHID()
+        {
+            return hostID;
+        }
+        public string getHName()
+        {
+            return hostName;
+        }
+        public int getHPNum()
+        {
+            return hostPropNum;
+        }
+        public double getLat()
+        {
+            return Lat;
+        }
+        public double getLong()
+        {
+            return Lat;
+        }
+        public string getRType()
+        {
+            return roomType;
+        }
+        public double getPrice()
+        {
+            return Price;
+        }
+        public int getMinNights()
+        {
+            return minNights;
+        }
+        public int getAvailDays()
+        {
+            return availableDays;
+        }
+
+        //Set Methods
+        public void setPID(string PID)
+        {
+            propID = PID;
+        }
+        public void setPN(string PN)
+        {
+            propName = PN;
+        }
+        public void setHID(string HID)
+        {
+            hostID = HID;
+        }
+        public void setHN(string HN)
+        {
+            hostName = HN;
+        }
+        public void setHPN(string HPN)
+        {
+            hostPropNum = Convert.ToInt32(HPN);
+        }
+        public void setLat(double L)
+        {
+            Lat = L;
+        }
+        public void setLongi(double Longi)
+        {
+            Long = Longi;
+        }
+        public void setRT(string RT)
+        {
+            roomType = RT;
+        }
+        public void setP(double P)
+        {
+            Price = P;
+        }
+        public void setminN(string minN)
+        {
+            minNights = Convert.ToInt32(minN);
+        }
+        public void setavailableD(string availableD)
+        {
+            availableDays = Convert.ToInt32(availableD);
+        }
+    }
+}
